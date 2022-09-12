d1fb90efae564cbc971ed69890a760274aa0051c
goporo <goporolo1@gmail.com>
2022-09-12 23:04:22 +0700

    doing navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
+import styles from './style'
+
+import { Stats, Business, Billing, CardDeal, Testimonials, Hero, Clients, CTA, Footer, Navbar } from './components/'
 
 const App = () => {
   return (
     <div className='bg-primary w-full overflow-hidden'>
-      <h1 className='text-5xl text-white'>HELLO HLELEOE HELEOO </h1>
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Navbar />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.paddingX} ${styles.boxWidth}`}>
+          <Stats />
+          <Business />
+          <Billing />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
     </div>
   )
 }
