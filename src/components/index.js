5af4e002e39b32e3db496c97198cc03fd226df11
goporo <goporolo1@gmail.com>
2022-09-13 22:26:38 +0700

    hero section

--- src/components/index.js
+++ src/components/index.js
@@ -8,3 +8,4 @@ export { default as Stats } from "./Stats";
 export { default as Footer } from "./Footer";
 export { default as Testimonials } from "./Testimonials";
 export { default as Hero } from "./Hero";
+export { default as GetStarted } from "./GetStarted";
